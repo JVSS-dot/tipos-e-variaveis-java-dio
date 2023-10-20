57466231da5c4a58a2b1b391e905c5599e9f8329
João Victor <jvss.trab@gmail.com>
2023-10-20 19:55:15 -0300

    testes da variavel int

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,7 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+    }
+}
