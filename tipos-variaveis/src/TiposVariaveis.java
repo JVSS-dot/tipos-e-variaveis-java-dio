e03a13f0621ebc0a9d8e13a92b7fa05ed97fd8bd
João Victor <jvss.trab@gmail.com>
2023-10-21 00:35:42 -0300

    fim dos estudos de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,7 +1,21 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        int numero = 5;
-        numero = 10;
-        System.out.println(numero);
+
+  public static void main(String[] args) throws Exception {
+    String primeiroNome = "Jorge ";
+    String segundoNome = "Vasconcelo";
+    String resultado = primeiroNome == segundoNome ? "verdadeiro" : "falso";
+    System.out.println("O nome é: " + resultado);
+
+
+    boolean a = true;
+    boolean b = false;
+
+    if (a || b) {
+        System.out.println("Uma das condições é verdadeira");
     }
+    if (b && (7 > 2)) {
+        System.out.println("condição de teste");
+    }
+
+  }
 }
